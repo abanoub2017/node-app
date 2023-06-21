2a376aac69f94ac4a3bcd0ed8071e9d83be951ef
abanoubgeorgefaw <Abanoubgeorge@totira.com>
2023-06-21 11:42:33 +0300

    handel error

--- app/src.js
+++ app/src.js
@@ -14,6 +14,20 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors())
 
+// Define a route to handle AI data
+app.get('', (req, res) => {
+    // Your AI data processing logic here
+    
+    // Sample response data
+    const aiData = {
+      message: 'This is AI data',
+      timestamp: new Date().toISOString()
+    };
+    
+    // Send the AI data as JSON response
+    res.json(aiData);
+  });
+
 
 const userRoutes = require('../routes/user');
 app.use("/user" , userRoutes)
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const http = require('http');
 
 const hostname = 'vita-team'
-const PORT = process.env.PORT | 3000
+const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Zeet Node')
 })
+
+
 const app = require("./app/src")
 // app.listen(PORT , () => console.log(`http://localhost:${PORT}`))
 app.listen(PORT, () => {
+    // console.log(window.URL);
     console.log(`server running at ${PORT}/`)
 });
